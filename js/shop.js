5dce8a85162a4195110331dde3231b189af527c2
Mika7596 <mika7596@gmail.com>
2025-02-22 13:23:42 +0100

    Defined some new variables referrign to elements in DOM and added the "buy" function

--- js/shop.js
+++ js/shop.js
@@ -1,20 +1,27 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-
-
-// => Reminder, it's extremely important that you debug your code. 
-// ** It will save you a lot of time and frustration!
-// ** You'll understand the code better than with console.log(), and you'll also find errors faster. 
-// ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
-
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
-
 var total = 0;
+const spanTotal = document.getElementById( "total_price" );
+spanTotal.innerText = total;
+const cartList = document.getElementById("cart_list");
+let countSpan = document.getElementById("count_product");
 
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array
+    let product = products.find(p => p.id == id);
+    let idList = cart.map(p => p.id);
+    if (!idList.includes(id)){
+        product.quantity = 1;
+        cart.push(product);
+    }
+    else {
+        for (let p of cart){
+            if (p.id === id){
+                p.quantity ++;
+            }
+        }
+     }
+     countSpan.innerHTML = cart.length;
 }
 
 // Exercise 2
